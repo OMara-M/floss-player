ba425a1a3abc58816d3fdf7777ebd867a488ed4c
Matthew O'Mara <tuh39725@temple.edu>
2022-10-30 18:03:53 -0400

    add function to create book list in main activity

--- app/src/main/java/com/example/flossplayer/BookList.kt
+++ app/src/main/java/com/example/flossplayer/BookList.kt
@@ -1,6 +1,18 @@
 package com.example.flossplayer
 
+import android.os.Parcel
+
 class BookList {
 
     private val myBookList = ArrayList<Book>()
+
+    fun add(book: Book){
+        myBookList.add(book)
+    }
+    fun remove(book: Book){
+        myBookList.remove(book)
+    }
+    fun get(){}
+    fun size(){}
+
 }
--- app/src/main/java/com/example/flossplayer/MainActivity.kt
+++ app/src/main/java/com/example/flossplayer/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    private fun createBookList(): BookList{
+        val books = resources.getStringArray(R.array.books)
+        val authors = resources.getStringArray(R.array.authors)
+
+        val
     }
 }
