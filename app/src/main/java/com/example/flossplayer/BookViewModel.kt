18450410dfb0c87622fb2baaac41650e1009195b
Matthew O'Mara <tuh39725@temple.edu>
2022-10-30 18:36:33 -0400

    add seter and getter for book objects

--- app/src/main/java/com/example/flossplayer/BookViewModel.kt
+++ app/src/main/java/com/example/flossplayer/BookViewModel.kt
@@ -1,7 +1,21 @@
 package com.example.flossplayer
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class BookViewModel: ViewModel() {
+
+    private val selectedBook: MutableLiveData<Book> by lazy {
+        MutableLiveData<Book>()
+    }
+
+    fun setSelectedBook(book:Book){
+        selectedBook.value + book
+    }
+
+    fun getSelectedBook(): LiveData<Book>{
+        return selectedBook
+    }
 
 }
